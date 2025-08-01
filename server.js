e273366225765a2c79afddcb17972dafe422748d
JSchut96 <jeroen.schut@hotmail.com>
2025-08-01 16:39:29 +0200

    Removed server header logging

--- server.js
+++ server.js
@@ -4,16 +4,6 @@ import path from 'path';
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('--- INCOMING REQUEST ---');
-  console.log('Host:', req.headers.host);
-  console.log('Origin:', req.headers.origin);
-  console.log('Referer:', req.headers.referer);
-  console.log('X-Forwarded-Proto:', req.headers['x-forwarded-proto']);
-  console.log('------------------------');
-  next();
-});
-
 app.set('trust proxy', true);
 
 app.use(express.static(path.resolve('./build')));
